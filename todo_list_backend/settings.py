182651ead0a95189f9137252881c5ece5f5e2b01
Bernhard Sigl <b-sigl@live.de>
2024-05-05 13:57:27 +0200

    customAuthToken

--- todo_list_backend/settings.py
+++ todo_list_backend/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'django-insecure-)(!)=2$z=%$f@8%3-5ouk=r9^%!x8k@nrm6mszpm-l%f%mn+40
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    # '127.0.0.1',
+    # 'localhost'
+]
+
+CORS_ALLOWED_ORIGINS =[
+    'http://localhost:4200', # frontend
+    ]
 
 
 # Application definition
@@ -38,10 +45,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'todo_list'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +132,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# self
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
