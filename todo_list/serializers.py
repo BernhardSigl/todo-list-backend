55ba1a5b7c26081eee87f6cb026d3e2314802ae5
Bernhard Sigl <b-sigl@live.de>
2024-05-08 01:00:44 +0200

    add new item; delete item; update item possibility

--- todo_list/serializers.py
+++ todo_list/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import TodoItem
 
 class TodoItemSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateField(required=False)
     class Meta:
         model = TodoItem
         fields = '__all__'
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import authentication, permissions
+from rest_framework import authentication, permissions, status
 
 from todo_list.models import TodoItem
 from todo_list.serializers import TodoItemSerializer
@@ -19,7 +18,34 @@ class TodoItemView(APIView): # Klasse immer wie das Model benennen
         serializer = TodoItemSerializer(todos, many=True)
         
         return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = TodoItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request):
+        todo_id = request.data.get('id')
+        todo_item = TodoItem.objects.get(id=todo_id)
+        print('test', todo_item)
+        todo_item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def put(self, request, format=None):
+        todo_id = request.data.get('id')
+        todo_item = TodoItem.objects.get(id=todo_id)
+        
+        updated_data = {
+            'title': request.data.get('title') + ' (bearbeitet)'
+        }
+        serializer = TodoItemSerializer(todo_item, data=updated_data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 class LoginView(ObtainAuthToken):
 
     def post(self, request, *args, **kwargs):
